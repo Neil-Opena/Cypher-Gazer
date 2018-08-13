44211c49fd3f947b56ccd5f8c9b3d24f850d87d4
Neil Opena <neil1114o@gmail.com>
2018-08-13 18:56:36 -0400

    Initialized project

---
+++ app/src/main/java/neil_opena/cyphergazer/CypherGazerActivity.java
@@ -0,0 +1,13 @@
+package neil_opena.cyphergazer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CypherGazerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cypher_gazer);
+    }
+}
