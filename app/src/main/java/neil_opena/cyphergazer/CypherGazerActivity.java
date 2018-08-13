96463cc1ff7404f97667519d81679221796315b7
Neil Opena <neil1114o@gmail.com>
2018-08-13 19:58:15 -0400

    Initialized fragment and added material dependency

--- app/src/main/java/neil_opena/cyphergazer/CypherGazerActivity.java
+++ app/src/main/java/neil_opena/cyphergazer/CypherGazerActivity.java
@@ -1,7 +1,10 @@
 package neil_opena.cyphergazer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class CypherGazerActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class CypherGazerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cypher_gazer);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = CypherGazerFragment.newInstance();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
---
+++ app/src/main/java/neil_opena/cyphergazer/CypherGazerFragment.java
@@ -0,0 +1,27 @@
+package neil_opena.cyphergazer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CypherGazerFragment extends Fragment{
+
+    public static CypherGazerFragment newInstance(){
+        return new CypherGazerFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        return v;
+    }
+}
